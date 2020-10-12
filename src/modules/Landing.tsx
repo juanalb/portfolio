28127a32141502022cf4bb60e1ab6c0805daeb72
juanalb <juan@zimmermanzimmerman.com>
2020-10-12 03:06:58 +0200

    chore: updated todo list

--- src/modules/Landing.tsx
+++ src/modules/Landing.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Background } from "../components/landing/Background";
 import { Typography } from "../components/landing/Typography";
 
-// TODO: make components render individually
-// TODO: make box-shadow follow the pattern/curve of the bg image
+// TODO: make components render individually DONE
+// TODO: make box-shadow follow the pattern/curve of the bg image DONE
 // TODO: destructurize components DONE
-// TODO: implement nice typograhy
+// TODO: implement nice typograhy DONE
 // TODO: implement scroll down cta
 
 export function Landing() {
